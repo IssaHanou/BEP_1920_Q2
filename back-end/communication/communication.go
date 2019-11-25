79845728ba5582ab32cba2310ba7ed851eb80b8b
Marijn de Schipper <M.deSchipper@student.tudelft.nl>
2019-11-25 17:19:56 +0100

    basic multiple topic subscription

---
+++ back-end/communication/communication.go
@@ -0,0 +1,56 @@
+package communication
+
+import (
+	"fmt"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	logger "github.com/sirupsen/logrus"
+	"time"
+)
+
+type communicator struct {
+	clientOptions    mqtt.ClientOptions
+	topicsOfInterest []string
+}
+
+func NewCommunicator(host string, port string, topicsOfInterest []string) *communicator {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("%s://%s:%s", "tcp", host, port))
+	opts.SetClientID("back-end")
+	opts.SetConnectionLostHandler(onConnectionLost)
+	return &communicator{*opts, topicsOfInterest}
+}
+
+func (communicator *communicator) Start() {
+	client := mqtt.NewClient(&communicator.clientOptions)
+	client.Connect()
+	topics := make(map[string]byte)
+	for _, topic := range communicator.topicsOfInterest {
+		topics[topic] = byte(0)
+	}
+	for {
+		token := client.SubscribeMultiple(topics, onIncomingDataReceived)
+		if token.Wait() && token.Error() != nil {
+			logger.Error("Fail to sub... ", token.Error())
+			time.Sleep(1 * time.Second)
+
+			logger.Errorf("Retry to subscribe")
+			continue
+		} else {
+			logger.Info("Subscribe successful!")
+			break
+		}
+	}
+}
+
+func onConnectionLost(client mqtt.Client, e error) {
+	logger.Warn(fmt.Sprintf("Connection lost : %v", e))
+	if client.IsConnected() {
+		client.Disconnect(500)
+	}
+
+	// Todo try to reconnect
+}
+
+func onIncomingDataReceived(client mqtt.Client, message mqtt.Message) {
+	logger.Info(string(message.Payload()))
+}
--- back-end/main.go
+++ back-end/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/IssaHanou/BEP_1920_Q2/back-end/config"
+	"./communication"
+	"time"
 )
+
+const host string = "localhost"
+const port string = "1883"
+var topics = []string{"back-end", "status", "hint"}
 
 func main() {
 	fmt.Println("Starting server")
@@ -12,4 +18,11 @@ func main() {
             "duration": "00:30:00"
     }`)
 	fmt.Println(config.GetFromJSON(data))
+
+	communicator := communication.NewCommunicator(host, port, topics)
+	communicator.Start()
+	for {
+		time.Sleep(time.Microsecond * time.Duration(250))
+	}
 }
+
