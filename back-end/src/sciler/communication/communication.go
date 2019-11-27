427d17dc78fe3cc39fbb492206d9640c601f12b0
Marijn de Schipper <M.deSchipper@student.tudelft.nl>
2019-11-27 15:17:38 +0100

    basic setup for testing Moved message handler

--- back-end/src/sciler/communication/communication.go
+++ back-end/src/sciler/communication/communication.go
@@ -1,6 +1,7 @@
 package communication
 
 import (
+	"errors"
 	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	logger "github.com/sirupsen/logrus"
@@ -25,14 +26,14 @@ func NewCommunicator(host string, port string, topicsOfInterest []string) *Commu
 }
 
 // Start is a function that will start the communication by connecting to the broker and subscribing to all topics of interest
-func (communicator *Communicator) Start() {
-	action(communicator.client.Connect, "connect", -1)
+func (communicator *Communicator) Start(handler mqtt.MessageHandler) {
+	_ = action(communicator.client.Connect, "connect", -1)
 	topics := make(map[string]byte)
 	for _, topic := range communicator.topicsOfInterest {
 		topics[topic] = byte(0)
 	}
-	action(func() mqtt.Token {
-		return communicator.client.SubscribeMultiple(topics, onIncomingDataReceived)
+	_ = action(func() mqtt.Token {
+		return communicator.client.SubscribeMultiple(topics, handler)
 	}, "subscribing", -1)
 }
 
@@ -45,33 +46,35 @@ func onConnectionLost(client mqtt.Client, e error) {
 	// Todo try to reconnect
 }
 
-func onIncomingDataReceived(client mqtt.Client, message mqtt.Message) {
-	logger.Info(string(message.Payload()))
-}
-
 // Publish is a method that will send a message to a specific topic
 func (communicator *Communicator) Publish(topic string, message string) {
-	action(func() mqtt.Token {
+	err := action(func() mqtt.Token {
 		return communicator.client.Publish(topic, byte(0), false, message)
 	}, "publish", 3)
+	if err == errors.New("action failed") {
+
+	}
 }
 
 // action is a function that will execute a communication action to the broker
 // actionType is the description of the action in one word for logging
 // retrials is the maximum number of times the action re-executed when failing, when retrials < 0, it is tried forever
-func action(action func() mqtt.Token, actionType string, retrials int) {
+func action(action func() mqtt.Token, actionType string, retrials int) error {
 	for i := 0; i < retrials || retrials < 0; i++ {
 		token := action()
+		var err error = nil
 		if token.Wait() && token.Error() != nil {
 			logger.Warnf("Fail to %s, %v", actionType, token.Error())
 			time.Sleep(1 * time.Second)
 
 			logger.Infof("Retry %d to %s", i +1, actionType)
+			err = errors.New("action eventually successful")
 			continue
 		} else {
 			logger.Infof("%s successful!", actionType)
-			return
+			return err
 		}
 	}
 	logger.Errorf("All retries to %s failed, giving up :(", actionType)
+	return errors.New("action failed")
 }
--- back-end/src/sciler/communication/communication_test.go
+++ back-end/src/sciler/communication/communication_test.go
@@ -1,1 +1,130 @@
 package communication
+
+import (
+	"fmt"
+	"github.com/eclipse/paho.mqtt.golang"
+	"github.com/stretchr/testify/mock"
+	"gotest.tools/assert"
+	"testing"
+	"time"
+)
+
+func TestNewCommunicator(t *testing.T) {
+	type args struct {
+		host             string
+		port             string
+		topicsOfInterest []string
+	}
+
+	optionsLocal := mqtt.NewClientOptions()
+	optionsLocal.AddBroker(fmt.Sprintf("%s://%s:%s", "tcp", "localhost", "1883"))
+	optionsLocal.SetClientID("back-end")
+	optionsLocal.SetConnectionLostHandler(onConnectionLost)
+
+	tests := []struct {
+		name string
+		args args
+		want *Communicator
+	}{
+		{
+			name: "two topics",
+			args: args{
+				host:             "localhost",
+				port:             "1883",
+				topicsOfInterest: []string{"test", "back-end"},
+			},
+			want: &Communicator{
+				clientOptions:    *optionsLocal,
+				client:           mqtt.NewClient(optionsLocal),
+				topicsOfInterest: []string{"test", "back-end"},
+			},
+		},
+		{
+			name: "no topics",
+			args: args{
+				host:             "localhost",
+				port:             "1883",
+				topicsOfInterest: nil,
+			},
+			want: &Communicator{
+				clientOptions:    *optionsLocal,
+				client:           mqtt.NewClient(optionsLocal),
+				topicsOfInterest: nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewCommunicator(tt.args.host, tt.args.port, tt.args.topicsOfInterest)
+			assert.DeepEqual(t, got.topicsOfInterest, tt.want.topicsOfInterest)
+		})
+	}
+}
+
+type TokenMock struct {
+	mock.Mock
+}
+
+func (t TokenMock) Wait() bool {
+	return false
+}
+
+func (t TokenMock) WaitTimeout(time.Duration) bool {
+	return true
+}
+
+func (t TokenMock) Error() error {
+	return nil
+}
+
+type ClientMock struct {
+	mock.Mock
+}
+
+func (c ClientMock) IsConnected() bool {
+	return true
+}
+
+func (c ClientMock) IsConnectionOpen() bool {
+	return true
+}
+
+func (c ClientMock) Connect() mqtt.Token {
+	return new(TokenMock)
+}
+
+func (c ClientMock) Disconnect(quiesce uint) {
+	return
+}
+
+func (c ClientMock) Publish(topic string, qos byte, retained bool, payload interface{}) mqtt.Token {
+	return new(TokenMock)
+}
+
+func (c ClientMock) Subscribe(topic string, qos byte, callback mqtt.MessageHandler) mqtt.Token {
+	return new(TokenMock)
+}
+
+func (c ClientMock) SubscribeMultiple(filters map[string]byte, callback mqtt.MessageHandler) mqtt.Token {
+	return new(TokenMock)
+}
+
+func (c ClientMock) Unsubscribe(topics ...string) mqtt.Token {
+	return new(TokenMock)
+}
+
+func (c ClientMock) AddRoute(topic string, callback mqtt.MessageHandler) {
+	return
+}
+
+func (c ClientMock) OptionsReader() mqtt.ClientOptionsReader {
+	return *new(mqtt.ClientOptionsReader)
+}
+
+func TestCommunicator_Start(t *testing.T) {
+// Todo: Add test
+}
+
+func TestCommunicator_Publish(t *testing.T) {
+// Todo: Add test
+}
