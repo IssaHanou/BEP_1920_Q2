49325a649e7ab508c4bc8159c0dddf45a3bf53c2
Issa <i.k.hanou@student.tudelft.nl>
2019-12-12 10:47:40 +0100

    styled device table with material design

--- front-end/src/app/components/device/device.component.ts
+++ front-end/src/app/components/device/device.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { Device } from "./device";
 import { AppComponent } from "../../app.component";
+import { MatSort, MatTableDataSource } from "@angular/material";
 
 @Component({
   selector: "app-device",
@@ -8,19 +9,64 @@ import { AppComponent } from "../../app.component";
   styleUrls: ["./device.component.css", "../../../assets/css/main.css"]
 })
 export class DeviceComponent implements OnInit {
-  deviceColumns: string[] = ['device', 'con-status', 'ond-status'];
-  deviceData = this.app.deviceList.all.values();
+  deviceColumns: string[] = ['id', 'connection', 'component', 'status'];
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor(private app: AppComponent) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
-  public getDeviceStatus(): Device[] {
+  /**
+   * Returns list of Device object with their current status and connection.
+   * Return in the form of map table data source, with sorting enabled.
+   */
+  public getDeviceStatus(): MatTableDataSource<Device> {
     const devices: Device[] = [];
     for (const device of this.app.deviceList.all.values()) {
       devices.push(device);
     }
     devices.sort((a: Device, b: Device) => a.id.localeCompare(b.id));
-    return devices;
+    let dataSource = new MatTableDataSource<Device>(devices);
+    dataSource.sort = this.sort;
+    return dataSource;
+  }
+
+  /**
+   * Creates list of components (keys of status maps), in alphabetical order.
+   * Returns string with each component's value on new line.
+   */
+  formatStatus(status: Map<string, any>) {
+    let keys = Array.from(status.keys());
+    keys.sort();
+
+    let result = "";
+    keys.forEach((key: string) => {
+      const value = status.get(key);
+      if (Array.isArray(value)) {
+        result += "[";
+        for (let i = 0; i < value.length; i++) {
+          result += value[i];
+          if (i < value.length - 1) {result += ","}
+        }
+        result += "]\n";
+      } else {
+        result += value + "\n";
+      }
+    });
+    return result;
+  }
+
+  /**
+   * Creates list of components (keys of status maps), in alphabetical order.
+   * Returns string with each component on new line.
+   */
+  getComponents(status: Map<string, any>) {
+    let keys = Array.from(status.keys());
+    keys.sort();
+    let result = "";
+    keys.forEach((key: string) => {result += key + "\n"});
+    return result;
   }
 }
