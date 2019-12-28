e889b1695145bcef7a6a77f7d273a71d74e8120b
Issa <i.k.hanou@student.tudelft.nl>
2019-12-28 14:09:07 +0100

    tested finish rule in handler

--- back-end/src/sciler/handler/handler_test.go
+++ back-end/src/sciler/handler/handler_test.go
@@ -858,6 +858,33 @@ func TestOnInstructionMsgHint(t *testing.T) {
 	communicatorMock.AssertNumberOfCalls(t, "Publish", 1)
 }
 
+func TestOnInstructionMsgFinishRule(t *testing.T) {
+	msg := Message{
+		DeviceID: "front-end",
+		TimeSent: "05-12-2019 09:42:10",
+		Type:     "instruction",
+		Contents: []map[string]interface{}{
+			{"instruction": "finish rule", "rule": "rule"},
+		},
+	}
+	communicatorMock := new(CommunicatorMock)
+	handler := Handler{
+		Config:       config.ReadFile("../../../resources/testing/test_instruction.json"),
+		Communicator: communicatorMock,
+	}
+	returnMessage, _ := json.Marshal(Message{
+		DeviceID: "back-end",
+		TimeSent: time.Now().Format("02-01-2006 15:04:05"),
+		Type:     "event status",
+		Contents: []map[string]interface{}{
+			{"id": "rule", "description": "My rule", "status": true},
+		},
+	})
+	communicatorMock.On("Publish", "front-end", string(returnMessage), 3)
+	handler.onInstructionMsg(msg)
+	communicatorMock.AssertNumberOfCalls(t, "Publish", 1)
+}
+
 func TestOnInstructionMsgMapper(t *testing.T) {
 	handler := getTestHandler()
 	msg := Message{
