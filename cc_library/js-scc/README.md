663189cba4c974a9c7ab391098f64dac540846a9
Marijn de Schipper <M.deSchipper@student.tudelft.nl>
2020-01-07 12:12:15 +0100

    start with npm module

---
+++ cc_library/js-scc/README.md
@@ -0,0 +1,9 @@
+### License
+GNU GENERAL PUBLIC LICENSE Version 3, [see `LICENSE.md`](LICENSE.md)
+
+### Setup
+install mqtt: ```npm install mqtt --save```
+install prettier: ```npm install --global prettier```
+
+### run prettier
+```prettier --write "**/*.js"```
--- cc_library/js-scc/scc.js
+++ cc_library/js-scc/scc.js
@@ -1,2 +1,29 @@
-var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://test.mosquitto.org');
+var mqtt = require("mqtt");
+var client = mqtt.connect("mqtt://192.168.178.82");
+
+client.on("connect", onConnect);
+client.on("message", onMessage);
+
+function onConnect() {
+  client.subscribe("test", function(err) {
+    if (!err) {
+      console.log("started")
+      publish("test", "I'm alive")
+    } else {
+      console.log(err)
+    }
+  });
+}
+
+function onMessage(topic, message) {
+  console.log(topic + " : " + message.toString());
+  client.end();
+}
+
+
+function publish(topic, message) {
+    client.publish(topic, message)
+}
+
+publish("test", "hello sccLib");
+
