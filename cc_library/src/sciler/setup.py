025dc716cbb5ecf28df58df118ba1d9ff041d124
Gwennan Smitskamp <G.M.Smitskamp@student.tudelft.nl>
2020-01-07 16:59:44 +0100

    updated version

--- cc_library/src/sciler/setup.py
+++ cc_library/src/sciler/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scclib",
-    version="0.0.4",
+    version="0.0.5",
     author="Raccoon Serious Games",
     author_email="BEP@raccoon.games",
     description="Client library for S.C.I.L.E.R.",
