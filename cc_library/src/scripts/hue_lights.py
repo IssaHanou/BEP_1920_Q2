52c327189b4b2974e11c8ee2666793384af9867d
Gwennan Smitskamp <G.M.Smitskamp@student.tudelft.nl>
2020-01-10 17:08:50 +0100

    ran black

--- cc_library/src/scripts/hue_lights.py
+++ cc_library/src/scripts/hue_lights.py
@@ -5,6 +5,7 @@ from scclib.device import Device
 
 import requests
 
+
 class Hue_lights(Device):
     def __init__(self):
         two_up = os.path.abspath(os.path.join(__file__, ".."))
@@ -30,53 +31,71 @@ class Hue_lights(Device):
 
     def test(self):
         print("test")
-        params = json.dumps({'on': True, 'bri': 200, "xy": [0.3,0.3]})
+        params = json.dumps({"on": True, "bri": 200, "xy": [0.3, 0.3]})
         headers = {"Content-type": "application/json"}
-        requests.put("http://192.168.178.128/api/JQrPwJNthHtfPEG9vhW3mqwIVuFo3ESLD3gvkZOB/groups/Spotlights/action", data=params, headers=headers)
+        requests.put(
+            "http://192.168.178.128/api/JQrPwJNthHtfPEG9vhW3mqwIVuFo3ESLD3gvkZOB/groups/Spotlights/action",
+            data=params,
+            headers=headers,
+        )
         time.sleep(2)
         params = json.dumps({"scene": self.scene})
         headers = {"Content-type": "application/json"}
-        resp = requests.put("http://192.168.178.128/api/JQrPwJNthHtfPEG9vhW3mqwIVuFo3ESLD3gvkZOB/groups/Spotlights/action", data=params, headers=headers)
+        resp = requests.put(
+            "http://192.168.178.128/api/JQrPwJNthHtfPEG9vhW3mqwIVuFo3ESLD3gvkZOB/groups/Spotlights/action",
+            data=params,
+            headers=headers,
+        )
         if resp.status_code == 200:
-            print('Template has been published.')
+            print("Template has been published.")
         else:
-            print('Unable to publish template.')
+            print("Unable to publish template.")
         self.status_changed()
 
     def set_scene(self, data):
         self.scene = data.get("value")
         params = json.dumps({"scene": self.scene})
         headers = {"Content-type": "application/json"}
-        resp = requests.put("http://192.168.178.128/api/JQrPwJNthHtfPEG9vhW3mqwIVuFo3ESLD3gvkZOB/groups/Spotlights/action", data=params, headers=headers)
+        resp = requests.put(
+            "http://192.168.178.128/api/JQrPwJNthHtfPEG9vhW3mqwIVuFo3ESLD3gvkZOB/groups/Spotlights/action",
+            data=params,
+            headers=headers,
+        )
         if resp.status_code == 200:
-            print('Template has been published.')
+            print("Template has been published.")
         else:
-            print('Unable to publish template.')
+            print("Unable to publish template.")
         self.status_changed()
 
     def set_manual(self, comp, data):
-        params = json.dumps({'on': data[0], 'bri': data[1], 'xy':data[2]})
+        params = json.dumps({"on": data[0], "bri": data[1], "xy": data[2]})
         headers = {"Content-type": "application/json"}
-        url = "http://192.168.178.128/api/JQrPwJNthHtfPEG9vhW3mqwIVuFo3ESLD3gvkZOB/lights/" + comp[-1:] + "/state"
+        url = (
+            "http://192.168.178.128/api/JQrPwJNthHtfPEG9vhW3mqwIVuFo3ESLD3gvkZOB/lights/"
+            + comp[-1:]
+            + "/state"
+        )
         print(url, params, comp[-1:])
         resp = requests.put(url, data=params, headers=headers)
         if resp.status_code == 200:
-            print('Template has been published.')
+            print("Template has been published.")
         else:
-            print('Unable to publish template.')
+            print("Unable to publish template.")
         self.status_changed()
 
     def reset(self):
         self.scene = "none"
-        params = json.dumps({'on': True, 'bri': 50, "xy": [0.3,0.3]})
+        params = json.dumps({"on": True, "bri": 50, "xy": [0.3, 0.3]})
         headers = {"Content-type": "application/json"}
         resp = requests.put(
-            "http://192.168.178.128/api/JQrPwJNthHtfPEG9vhW3mqwIVuFo3ESLD3gvkZOB/groups/Spotlights/action", data=params,
-            headers=headers)
+            "http://192.168.178.128/api/JQrPwJNthHtfPEG9vhW3mqwIVuFo3ESLD3gvkZOB/groups/Spotlights/action",
+            data=params,
+            headers=headers,
+        )
         if resp.status_code == 200:
-            print('Template has been published.')
+            print("Template has been published.")
         else:
-            print('Unable to publish template.')
+            print("Unable to publish template.")
         self.status_changed()
 
     def main(self):
