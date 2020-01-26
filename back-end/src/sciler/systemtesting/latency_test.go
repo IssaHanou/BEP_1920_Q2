9b3e5c62586a8309a9e62c58f0224fefa5f43ef3
Marijn de Schipper <m.deschipper567@gmail.com>
2020-01-26 17:46:02 +0100

    100 messages

--- back-end/src/sciler/systemtesting/latency_test.go
+++ back-end/src/sciler/systemtesting/latency_test.go
@@ -20,9 +20,11 @@ import (
 // Make sure when running windows, the setup in systemtesting/README.md is followed
 
 func TestLatency(t *testing.T) {
-	executeScript("test.sh")
-	executeScript("clientComputer.sh")
-	executeScript("localBroker.sh")
+	// start broker
+	// start latency client
+
+	receiveTimes := make(map[int]int64, 100)
+
 	dir, dirErr := os.Getwd()
 	if dirErr != nil {
 		logger.Fatal(dirErr)
@@ -31,21 +33,18 @@ func TestLatency(t *testing.T) {
 	// setup back-end
 	filename := filepath.Join(dir, "system_test_config.json")
 	configurations := config.ReadFile(filename)
-	logger.Info("configurations read from: " + filename)
-	host := configurations.General.Host
-	port := configurations.General.Port
-
-	var responseTime time.Time
+	logger.SetLevel(logger.ErrorLevel)
 
 	messageHandler := handler.Handler{Config: configurations, ConfigFile: filename}
-	messageHandler.Communicator = communication.NewCommunicator(host, port, []string{"back-end"}, func(client mqtt.Client, message mqtt.Message) {
+	messageHandler.Communicator = communication.NewCommunicator(configurations, func(client mqtt.Client, message mqtt.Message) {
 		var msg handler.Message
 		if err := json.Unmarshal(message.Payload(), &msg); err != nil {
 			logger.Errorf("invalid JSON received: %v", err)
 		}
 
-		if msg.Type == "confirmation" {
-			responseTime = time.Now()
+		if msg.Type == "status" {
+			contents := msg.Contents.(map[string]interface{})
+			receiveTimes[int(contents["ping"].(float64))] = makeTimestamp()
 		}
 
 	}, func() {
@@ -54,17 +53,40 @@ func TestLatency(t *testing.T) {
 	// start back-end
 	messageHandler.Communicator.Start()
 
-	// Send instruction to Client computer
-	requestTime := time.Now()
-	messageHandler.SendInstruction("latency", []map[string]string{{
-		"instruction":   "ping",
-		"instructed_by": "back-end",
-	}})
+	const numberOfMessage = 100
 
-	time.Sleep(time.Duration(1) * time.Second)
-	latency := responseTime.Sub(requestTime)
-	logger.Info(latency)
-	assert.True(t, latency.Round(time.Millisecond).Milliseconds() <= 100)
+	requestTimes := make(map[int]int64, 100)
+	for i := 0; i < numberOfMessage; i++ {
+
+		// Send instruction to Client computer
+		requestTimes[i] = makeTimestamp()
+		messageHandler.SendComponentInstruction("latency", []config.ComponentInstruction{{
+			ComponentID: "ping",
+			Instruction: "ping",
+			Value:       i,
+		}}, "")
+
+	}
+
+	time.Sleep(1 * time.Second)
+
+	var meanLatency float64
+
+	for i := 0; i < numberOfMessage; i++ {
+		assert.False(t, receiveTimes[i] == 0, "Did not receive an status message in time")
+		latency := receiveTimes[i] - requestTimes[i]
+		fmt.Println(receiveTimes[i])
+		fmt.Println(requestTimes[i])
+		fmt.Println(latency)
+		assert.Truef(t, latency <= 100, "the latency of all messages should be <= 100 ms but latency was %v ms", latency)
+		meanLatency += float64(latency) / numberOfMessage
+	}
+
+	fmt.Println(fmt.Sprintf("%d messages were sent with a mean latency of %v ms", numberOfMessage, meanLatency))
+}
+
+func makeTimestamp() int64 {
+	return time.Now().UnixNano() / (int64(time.Millisecond)/int64(time.Nanosecond))
 }
 
 func executeScript(script string) {
