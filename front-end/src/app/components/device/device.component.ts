f1af774f14cb2c779314b8198cfdbb62c58a23af
Issa <i.k.hanou@student.tudelft.nl>
2020-01-28 15:07:19 +0100

    created component expansion tables

--- front-end/src/app/components/device/device.component.ts
+++ front-end/src/app/components/device/device.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { Device } from "./device";
+import {Comp, Device} from "./device";
 import { AppComponent } from "../../app.component";
 import { MatSort, MatTableDataSource } from "@angular/material";
 
@@ -13,23 +13,20 @@ import { MatSort, MatTableDataSource } from "@angular/material";
 })
 export class DeviceComponent implements OnInit {
   /**
-   * The keys used by the table to retrieve data from the DataSource
+   * The keys used by the device table to retrieve data from the DataSource.
    */
-  deviceColumns: string[] = ["id", "connection", "unfold", "component", "status", "test"];
-
+  deviceColumns: string[] = ["id", "connection", "unfold", "test"];
   /**
-   * Map keeping track of which rows are collapsed.
+   * The keys used by the component table to retrieve data from the component list.
    */
-  collapsed: Map<string, boolean>;
+  componentColumns: string[] = ["component", "status"];
 
   /**
    * Control the sorting of the table.
    */
   @ViewChild("DeviceTableSort", { static: true }) sort: MatSort;
 
-  constructor(private app: AppComponent) {
-    this.collapsed = new Map<string, boolean>();
-  }
+  constructor(private app: AppComponent) {}
 
   ngOnInit() {}
 
@@ -41,15 +38,30 @@ export class DeviceComponent implements OnInit {
     const devices: Device[] = [];
     for (const device of this.app.deviceList.all.values()) {
       devices.push(device);
-      if (!this.collapsed.has(device.id)) {
-        this.collapsed.set(device.id, true);
-      }
     }
     devices.sort((a: Device, b: Device) => a.id.localeCompare(b.id));
 
     const dataSource = new MatTableDataSource<Device>(devices);
     dataSource.sort = this.sort;
     return dataSource;
+  }
+
+  /**
+   * Returns list of Comp objects with their current status.
+   * For the front-end, only return the game state not the button pressed statuses.
+   */
+  public getComponentList(deviceId: string): Comp[] {
+    const status = this.app.deviceList.getDevice(deviceId).status;
+    if (deviceId === "front-end") {
+      return [status.get("gameState")];
+    }
+    const ret = [];
+    const keys = Array.from(status.keys());
+    keys.sort();
+    keys.forEach((key: string) => {
+      ret.push(status.get(key));
+    });
+    return ret;
   }
 
   /**
@@ -64,64 +76,6 @@ export class DeviceComponent implements OnInit {
   }
 
   /**
-   * Creates list of components (keys of status maps), in alphabetical order.
-   * Returns string with each component on new line.
-   * If components are collapsed, return default.
-   * For the front-end, only show gameState.
-   */
-  getComponents(status: Map<string, any>, deviceId: string): any {
-    if (this.collapsed.get(deviceId)) {
-      return "";
-    } else if (deviceId === "front-end") {
-      return "gameState";
-    } else {
-      const keys = Array.from(status.keys());
-
-      keys.sort();
-      let result = "";
-      keys.forEach((key: string) => {
-        result += key + "\n";
-      });
-      return result;
-    }
-  }
-
-  /**
-   * Creates list of components (keys of status maps), in alphabetical order.
-   * Returns string with each component's value on new line.
-   * If components are collapse, return nothing.
-   * For the front-end only show gameState status.
-   */
-  formatStatus(status: Map<string, any>, deviceId: string): string {
-    if (this.collapsed.get(deviceId)) {
-      return "";
-    } else if (deviceId === "front-end") {
-      return status.get("gameState");
-    } else {
-      const keys = Array.from(status.keys());
-      keys.sort();
-
-      let result = "";
-      keys.forEach((key: string) => {
-        const value = status.get(key);
-        if (Array.isArray(value)) {
-          result += "[";
-          for (let i = 0; i < value.length; i++) {
-            result += value[i];
-            if (i < value.length - 1) {
-              result += ",";
-            }
-          }
-          result += "]\n";
-        } else {
-          result += value + "\n";
-        }
-      });
-      return result;
-    }
-  }
-
-  /**
    * When button is pressed, test a single device.
    * The test button can not be clicked when game is in play (timer is running).
    */
@@ -129,15 +83,5 @@ export class DeviceComponent implements OnInit {
     this.app.sendInstruction([
       { instruction: "test device", device: deviceId }
     ]);
-  }
-
-  /**
-   * Clicking the 'status' button should show the components and their statuses.
-   * Clicking the 'sluit' button should hide the components and their statuses.
-   * @param deviceID the status of this device should be shown/hidden
-   */
-  collapseComponents(deviceID: string) {
-    const oldValue = this.collapsed.get(deviceID);
-    this.collapsed.set(deviceID, !oldValue);
   }
 }
