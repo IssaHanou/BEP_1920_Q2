32549f8d42611139c74b37bd6f436d1750ec7641
Marijn de Schipper <m.deschipper567@gmail.com>
2020-02-01 13:20:33 +0100

    Fixed scanner

--- cc_library/example_scripts_py_scc/scanner.py
+++ cc_library/example_scripts_py_scc/scanner.py
@@ -1,5 +1,6 @@
 import os
 from sciler.device import Device
+from evdev import InputDevice, categorize, ecodes
 
 try:
     from evdev import InputDevice, categorize, ecodes
@@ -64,20 +65,12 @@ class Scanner(Device):
 
         def async_loop():
             while True:
-                self.code = eval(input())
+                # self.code = eval(input())
+                self.code = eval(read_barcode("/dev/input/event0"))
                 print(self.code)
                 self.scanned.append(self.code)
                 print(self.scanned)
                 self.status_changed()
-            # card_reader = NumpadReader(
-            #     result_handler=self.reader_handle_result,
-            #     status_change=self.reader_status_changed,
-            #     log=self.log,
-            #     usb_path="/dev/input/event0",
-            # )
-            # loop = asyncio.get_event_loop()
-            #
-            # loop.run_until_complete(asyncio.gather(card_reader.start()))
 
         self.start(loop=async_loop)
 
@@ -92,71 +85,55 @@ class Scanner(Device):
         self.status_changed()
 
 
-class NumpadReader:
-    def __init__(
-            self, result_handler, status_change, usb_path="/dev/input/event0", log=print
-    ):
-        self.word = ""
-        self.device = InputDevice(usb_path)
-        self.count = 0
-        self.handler = result_handler
-        self.statusChange = status_change
-        self.log = log
+scan_codes = {
+    # Scancode: ASCIICode
+    0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8',
+    10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP', 15: u'TAB', 16: u'q', 17: u'w', 18: u'e', 19: u'r',
+    20: u't', 21: u'y', 22: u'u', 23: u'i', 24: u'o', 25: u'p', 26: u'[', 27: u']', 28: u'CRLF', 29: u'LCTRL',
+    30: u'a', 31: u's', 32: u'd', 33: u'f', 34: u'g', 35: u'h', 36: u'j', 37: u'k', 38: u'l', 39: u';',
+    40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'z', 45: u'x', 46: u'c', 47: u'v', 48: u'b', 49: u'n',
+    50: u'm', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 57: u' ', 100: u'RALT'
+}
 
-    async def start(self):
-        self.log("Started listening for input")
-        async for event in self.device.async_read_loop():
-            if event.type == ecodes.EV_KEY:
-                data = categorize(event)
+caps_codes = {
+    0: None, 1: u'ESC', 2: u'!', 3: u'@', 4: u'#', 5: u'$', 6: u'%', 7: u'^', 8: u'&', 9: u'*',
+    10: u'(', 11: u')', 12: u'_', 13: u'+', 14: u'BKSP', 15: u'TAB', 16: u'Q', 17: u'W', 18: u'E', 19: u'R',
+    20: u'T', 21: u'Y', 22: u'U', 23: u'I', 24: u'O', 25: u'P', 26: u'{', 27: u'}', 28: u'CRLF', 29: u'LCTRL',
+    30: u'A', 31: u'S', 32: u'D', 33: u'F', 34: u'G', 35: u'H', 36: u'J', 37: u'K', 38: u'L', 39: u':',
+    40: u'\'', 41: u'~', 42: u'LSHFT', 43: u'|', 44: u'Z', 45: u'X', 46: u'C', 47: u'V', 48: u'B', 49: u'N',
+    50: u'M', 51: u'<', 52: u'>', 53: u'?', 54: u'RSHFT', 56: u'LALT', 57: u' ', 100: u'RALT'
+}
+
+
+def read_barcode(device_path):
+    dev = InputDevice(device_path)
+    dev.grab()  # grab provides exclusive access to the device
+
+    x = ''
+    caps = False
+
+    for event in dev.read_loop():
+        if event.type == ecodes.EV_KEY:
+            data = categorize(event)  # Save the event temporarily to introspect it
+            if data.scancode == 42:
                 if data.keystate == 1:
-                    value = data.scancode
-                    self.process_value(value)
+                    caps = True
+                if data.keystate == 0:
+                    caps = False
 
-    def process_value(self, value):
-        self.count = self.count + 1
-        number = self.get_value(value)
+            if data.keystate == 1:  # Down events only
+                if caps:
+                    key_lookup = u'{}'.format(caps_codes.get(data.scancode)) or u'UNKNOWN:[{}]'.format(
+                        data.scancode)  # Lookup or return UNKNOWN:XX
+                else:
+                    key_lookup = u'{}'.format(scan_codes.get(data.scancode)) or u'UNKNOWN:[{}]'.format(
+                        data.scancode)  # Lookup or return UNKNOWN:XX
 
-        if not self.check_enter(value):
-            if number is None:
-                return
+                if (data.scancode != 42) and (data.scancode != 28):
+                    x += key_lookup
 
-            self.word = self.word + str(number)
-            # self.statusChange(self.word)
-            return
-
-        # Could be something for the config
-
-        self.handler(self.word)
-
-        self.count = 0
-        self.word = ""
-
-    def check_enter(self, c):
-        return c == 28 or c == 96  # KEY_ENTER or KEY_KPENTER
-
-    def get_value(self, value):
-        mapping = {
-            102: 7,
-            71: 7,
-            103: 8,
-            72: 8,
-            104: 9,
-            73: 9,
-            75: 4,
-            105: 4,
-            76: 5,
-            106: 6,
-            77: 6,
-            107: 1,
-            79: 1,
-            108: 2,
-            80: 2,
-            109: 3,
-            81: 3,
-            110: 0,
-            82: 0,
-        }
-        return mapping.get(value, None)
+                if (data.scancode == 28):
+                    return x
 
 
 if __name__ == "__main__":
