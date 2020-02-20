a5044e6ea565838fd38f14dec1034b2e956e8eab
Gwennan Smitskamp <G.M.Smitskamp@student.tudelft.nl>
2020-02-20 15:51:00 +0100

    removed the debug millisecs

--- front-end/src/app/components/timer/timer.ts
+++ front-end/src/app/components/timer/timer.ts
@@ -37,7 +37,6 @@ export class Timer {
  * Format the time in milliseconds to a string in the format hh:mm:ss.
  */
 export function formatMS(timeInMS) {
-  const milseconds = parseInt(((timeInMS ) ).toString(), 10);
   const seconds = parseInt(((timeInMS / 1000) % 60).toString(), 10);
   const minutes = parseInt(((timeInMS / (1000 * 60)) % 60).toString(), 10);
   const hours = parseInt(((timeInMS / (1000 * 60 * 60)) % 24).toString(), 10);
@@ -45,7 +44,7 @@ export function formatMS(timeInMS) {
   const m = minutes < 10 ? "0" + minutes : minutes;
   const s = seconds < 10 ? "0" + seconds : seconds;
 
-  return h + ":" + m + ":" + s +":"+milseconds;
+  return h + ":" + m + ":" + s;
 }
 
 /**
