a433be0a076c7c7dd429a33cd576ec790c03543f
Issa <i.k.hanou@student.tudelft.nl>
2020-03-17 18:04:25 +0100

    renamed device packages

--- cc_library/example_scripts_js_scc/navigate-slides/e2e/src/app.e2e-spec.ts
+++ cc_library/example_scripts_js_scc/navigate-slides/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('navigate app is running!');
+    expect(page.getTitleText()).toEqual('navigate-slides app is running!');
   });
 
   afterEach(async () => {
